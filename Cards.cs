5c57001d430970c17493f6c6d59114e449d2a015
Reb <rebeldomaker@icloud.com>
2026-09-14 22:58:37 +0200

    create US2

--- Cards.cs
+++ Cards.cs
@@ -1,36 +1,68 @@
 namespace Blackjack;
 
-// TODO: Write a loop to populate the deck with all 52 unique Suit & Rank combinations
-// TODO: Call the deck creation code when the application starts in Main()
-// TODO: Finish the PrintDeck method to iterate through and display each card in the deck
-
-class Card
+class Cards
 {
     // TODO: Define properties (Suit, Rank, Value) inside the Card class
+    /* import random as rand
+        def card()
+            foreach i in deck range(52):
+                print(card-deck)*/
 
-    static void PrintDeck(List<Card> cards)
+    public string Rank { get; set; }
+    public string Suit { get; set; }
+    public int Value { get; set; }
+
+    public Cards(string rank, string suit, int value)
     {
-        string[] Rank = {
-            "Ace",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6",
-            "7",
-            "8",
-            "9",
-            "10",
-            "Jack",
-            "Queen",
-            "King"
+        Rank = rank;
+        Suit = suit;
+        Value = value;
+    }
+
+    public static List<Cards> CreateDeck()
+    {
+        List<Cards> deck = new List<Cards>();
+
+        string[] ranks = {
+            "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"
         };
 
-        string[] Suit = {
-            "♣",
-            "♦",
-            "♥",
-            "♠"
+        string[] suits = {
+            "♣", "♦", "♥", "♠"
         };
+
+        foreach (string suit in suits)
+        {
+            for (int i = 0; i < ranks.Length; i++)
+            {
+                string rank = ranks[i];
+                int value;
+
+                if (rank == "Ace")
+                {
+                    value = 11;
+                }
+                else if (rank == "Jack" || rank == "Queen" || rank == "King")
+                {
+                    value = 10;
+                }
+                else
+                {
+                    value = int.Parse(rank);
+                }
+
+                deck.Add(new Cards(rank, suit, value));
+            }
+        }
+
+        return deck;
+    }
+
+    public static void PrintDeck(List<Cards> cards)
+    {
+        foreach (Cards card in cards)
+        {
+            Console.WriteLine($"{card.Rank} of {card.Suit} (Value: {card.Value})");
+        }
     }
 }
--- MainGame.cs
+++ MainGame.cs
@@ -8,9 +8,21 @@
             Console.ReadLine(); // press enter to then print new text aka the line of code below this comment
             Console.Clear();
             Console.WriteLine("- Blackjack 2026 project, for ZBC -");
+            
+            // Creates the 52-card deck
+            List<Cards> deck = Cards.CreateDeck(); // deck = Cards.create_deck()
+
+            // Prints all 52 cards to the console
+            Cards.PrintDeck(deck);
         }
         
         // TODO: Create a List<Card> deck collection in MainGame.cs
+
+        // TODO: Call the deck creation code when the application starts in Main()
+
+        // TODO: Finish the PrintDeck method to iterate through and display each card in the deck
+        
+        // TODO: Write a loop to populate the deck with all 52 unique Suit & Rank combinations
 
     }
 }
