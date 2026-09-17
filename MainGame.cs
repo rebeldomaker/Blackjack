c1e16759809d1df6c7a5df311706559add12e63c
Reb <rebeldomaker@icloud.com>
2026-09-17 22:26:43 +0200

    ai removed mentions of "cite", fixed other quirks?

--- MainGame.cs
+++ MainGame.cs
@@ -11,28 +11,28 @@
             Console.WriteLine("- Blackjack 2026 project, for ZBC -");
             
             // Creates the 52-card deck
-            List<Cards> deck = Cards.CreateDeck();[cite: 1]
+            List<Cards> deck = Cards.CreateDeck();
 
             // Create a List<Cards> playerHand collection to represent the player's hand (initially empty)
-            List<Cards> playerHand = new List<Cards>();[cite: 2]
+            List<Cards> playerHand = new List<Cards>();
 
             // Call Cards.DrawCard(deck) twice and add both drawn cards to playerHand
             for (int i = 0; i < 2; i++) 
             {
-                Cards drawnCard = Cards.DrawCard(deck);[cite: 1, 2]
-                playerHand.Add(drawnCard);[cite: 2]
+                Cards drawnCard = Cards.DrawCard(deck);
+                playerHand.Add(drawnCard);
             }
                
             // Display both cards in playerHand to the console using a loop or individual WriteLine statements
             foreach (Cards card in playerHand)
             {
-                Console.WriteLine($"You drew {card.Rank} of {card.Suit}");[cite: 1, 2]
+                Console.WriteLine($"You drew {card.Rank} of {card.Suit}");
             }
 
             // Verify playerHand.Count equals 2 to confirm the initial deal is complete
-            if (playerHand.Count == 2)[cite: 2]
+            if (playerHand.Count == 2)
             {
-                Console.WriteLine("Initial deal complete!");[cite: 2]
+                Console.WriteLine("Initial deal complete!");
             }
 
             // Calculate initial score
@@ -40,8 +40,8 @@
             int initialAces = 0;
             foreach (Cards card in playerHand)
             {
-                initialScore += card.Value;[cite: 1]
-                if (card.Rank == "Ace")[cite: 1]
+                initialScore += card.Value;
+                if (card.Rank == "Ace")
                 {
                     initialAces++;
                 }
